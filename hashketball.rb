0f5bfb818fd62f4a4cf6c34756ebdf8953201e70
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-08 18:57:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -78,8 +78,8 @@ end
 def player_stats(player_name)
   game_hash.each do |key, value|
     value[:players].each do |player|
-      if player == player_name
-        return player
+      if player[:player_name] == player_name
+        return player[:player_name]
       end
     end
   end
